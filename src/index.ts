029add9c098811203aeca4509f78b5ec5f3f7e3e
dualwind <dualwind@qq.com>
2023-08-14 23:50:18 +0800

    初始化项目

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import { Plugin } from "siyuan";
-
+import { getBlockAttrs } from "../../siyuanPlugin-common/siyuan-api";
 export default class PluginHotkey extends Plugin {
-  async onload() {
+  private blockId: BlockId;
+  private setBlockIdThis = this.setBlockId.bind(this);
+  async onload() {}
+
+  onLayoutReady() {
+    this.eventBus.on("click-editorcontent", this.setBlockIdThis);
     this.addCommand({
-      langKey: "showDialog",
-      hotkey: "⇧⌘M",
-      callback: () => {
-        this.showDialog();
+      langKey: "设置命名",
+      hotkey: "⌘1",
+      callback: async () => {
+        const id = this.blockId;
+        let attrs = await getBlockAttrs(id);
+        const content=
+        console.log(attrs);
       },
     });
   }
 
-  onLayoutReady() {}
-
-  onunload() {}
+  onunload() {
+    this.eventBus.off("click-editorcontent", this.setBlockIdThis);
+  }
+  setBlockId({ detail }) {
+    this.blockId = detail.protyle.breadcrumb?.id;
+  }
 }
